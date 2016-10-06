d12e8706c3643f634ae7d825881618722c10dd74
cavossman <cvossman@ksu.edu>
2016-10-05 21:34:01 -0500

    Created grid and added files for sound.

--- bundle.js
+++ bundle.js
@@ -9,13 +9,27 @@ var canvas = document.getElementById('screen');
 var game = new Game(canvas, update, render);
 var image = new Image();
 image.src = 'assets/pipes.png';
+var grid = new Array(144);
 
-// TODO: Place the pipe tiles on the board in random order
+
+// SOUNDS
+//var backgroundSound = new Audio();
+//backgroundSound.src = 'assets/backgroundSound.wav';
+//backgroundSound.loop = true;
+//backgroundSound.volume = 0.3;
+//backgroundSound.play();
+var win = new Audio();
+win.src = 'assets/win.wav';
+var lose = new Audio();
+lose.src = 'assets/lose.wav';
+var place = new Audio();
+place.src = 'assets/place.wav';
+var rotate = new Audio();
+rotate.src = 'assets/rotate.wav';
 
 canvas.onclick = function(event) {
   event.preventDefault();
-  // TODO: determine which pipe tile was clicked on
-  // TODO: rotate the pipes in the pipe tile
+  // TODO: Place or rotate pipe tile
 }
 
 /**
@@ -55,7 +69,26 @@ function render(elapsedTime, ctx) {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   // TODO: Render the board
-
+  ctx.fillStyle = 'black';
+  ctx.fillRect(720,0,120,720);
+  ctx.fillStyle = 'white';
+  ctx.fillRect(722,2,116,716);
+  
+  
+  var y = 0;
+  var x = 0;
+  for (var i = 0; i < grid.length; i++){
+	if(i >= 11 && i % 12 == 0){
+		y += 60;
+		x = 0;
+	}
+	ctx.fillStyle = 'black';
+	ctx.fillRect(x,y,60,60);
+	ctx.fillStyle = "grey";
+	ctx.fillRect(x+2,y+2,56,56)
+	x += 60;
+  }
+  
 }
 
 },{"./game":2}],2:[function(require,module,exports){
